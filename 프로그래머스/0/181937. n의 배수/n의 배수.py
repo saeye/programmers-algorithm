5a9a41127b35c2098ae62c868657d3fb30a011e4
Saeye Lee <62229734+saeye@users.noreply.github.com>
2025-04-17 17:48:58 +0900

    [level 0] Title: n의 배수, Time: 0.04 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/0/181937. n의 배수/n의 배수.py
+++ 프로그래머스/0/181937. n의 배수/n의 배수.py
@@ -2,3 +2,13 @@ def solution(num, n):
     if num % n == 0:
         return 1
     return 0
+
+'''
+int(True)는 1, int(False)는 0 이니까
+
+def solution(num, n):
+    return int(num % n == 0)
+
+이렇게 쓰면 더 깔끔하다.
+
+'''
