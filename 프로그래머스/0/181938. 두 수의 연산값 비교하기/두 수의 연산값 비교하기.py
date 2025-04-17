3bed892a26ba932ae2abd813199e02b75872b534
Saeye Lee <62229734+saeye@users.noreply.github.com>
2025-04-17 17:31:53 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.03 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.py
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.py
@@ -1,6 +1,10 @@
-def solution(a, b):
-    ab_connect = int(str(a)+str(b))
-    ab = 2*a*b
-    if ab_connect >= ab:
-        return ab_connect
-    return ab
+# def solution(a, b):
+#     ab_connect = int(str(a)+str(b))
+#     ab = 2*a*b
+#     if ab_connect >= ab:
+#         return ab_connect
+#     return ab
+
+def solution(a,b):
+    return max(int(str(a)+str(b)), 2*a*b)
+    
